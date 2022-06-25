485d29b25fef00036f7507fda0c57aae148c0b15
Cameron Grande <81730723+camisbored@users.noreply.github.com>
2022-06-25 19:40:21 -0400

    Updated documentation

--- resources/PageServer.java
+++ resources/PageServer.java
@@ -1,3 +1,13 @@
+/*
+PageServer.java
+@author Cameron Grande
+A fully functioning http/application server.
+Since we are aiming for simplicity and configurability,
+all the logic exists inside this single Java file.
+Other examples and practical application exists
+alongside in this folder.
+*/
+
 import java.io.*;
 import java.util.regex.*;
 import java.lang.reflect.Method;
@@ -10,6 +20,7 @@ import java.util.Date;
 import java.util.HashMap;
 
 public class PageServer{
+        //Init static variables used throughout server
 	static HashMap<String, String> contentTypeMap = new HashMap<String, String>();
 	static String generatedDirectory = System.getProperty("user.dir");
 	static String rootDirectory = new File(generatedDirectory).getParent();
@@ -23,22 +34,26 @@ public class PageServer{
 		contentTypeMap.put("ico", "image/x-icon");
 		contentTypeMap.put("jpg", "image/*");
 	}
+
    public static void main(String[] args) throws Exception {
+    //If port not specified through command line arg, default to 80
     int port = 80;
     if (args.length==1)
     	port = Integer.parseInt(args[0]);
     ServerSocket serverSocket = new ServerSocket(port);
     log("Server launched on port : " + port);
 
+    //Until server is terminated, wait for new connections and start new thread for each
     while (true) {
 	Socket clientSocket = serverSocket.accept();
-    ExecutorService executor = Executors.newSingleThreadExecutor();
-    executor.submit(()->{
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.submit(()->{
 	     try{ 
 		String firstString = "";
 	        log("New Connection from "+ clientSocket.getInetAddress());
 	        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 	        String s;
+                //Parse incoming object and handle l
 	        Request request = new Request();
 	        while ((s = in.readLine()) != null) {
 			    if (firstString.isEmpty())
@@ -60,13 +75,14 @@ public class PageServer{
 	        	request.setContent(postData.toString());
 	        }
 	        String requestedResource = firstString.split("\\s+")[1];
+                        //If get request contains query
 			if (requestedResource.contains("?")){
 				request.setQuery(requestedResource.split("\\?")[1]);
 				request.setResourceRequested(requestedResource.split("\\?")[0]);
 			}
 			else 
 				request.setResourceRequested(requestedResource);
-			
+			//If index page, override
 			if (request.getResourceRequested().equals("/"))
 				request.setResourceRequested("index.html");
 
@@ -76,6 +92,7 @@ public class PageServer{
 				request.setFileExtension(request.getResourceRequested().split("\\.")[1]);
 				request.setResourceRequested(request.getResourceRequested().split("\\.")[0]);
 			}
+                        //Check for supported methods, kick otherwise
 			if (request.getMethod().equals("GET") || request.getMethod().equals("POST")) {
 				String nameForJavaCheck = request.getResourceRequested();
 				if (!nameForJavaCheck.equals("index"))
@@ -94,6 +111,7 @@ public class PageServer{
 				displayErrorPage("405", "Method Not Allowed", request, clientSocket);
 				clientSocket.close();
 			}
+                //Interaction complete- close connection
 	        log("Connection with client completed");
 	        in.close();
 	        clientSocket.close();
@@ -104,6 +122,12 @@ public class PageServer{
     }
 }
    
+/**
+ * defaultRequestProcess- handles the default scenario of loading an page
+ * @param request- the current session request
+ * @param clientSocket- current TCP client instance
+ * @throws IOException- if invalid I/O
+ */
 public static void defaultRequestProcess(Request request, Socket clientSocket) throws IOException {
 	String path = resourceDirectory+File.separator+request.getResourceRequested()+"."+request.getFileExtension();
 	File file;
@@ -132,6 +156,14 @@ public static void defaultRequestProcess(Request request, Socket clientSocket) t
 	input.close();
 }
 
+/**
+ * displayErrorPage- loads error page and replaced with provided map
+ * @param statusCode- HTTP status code as strong
+ * @param errorMessage- error message describing what error took place.
+ * @param request- the current session request
+ * @param clientSocket- current TCP client instance
+ * @throws IOException- if invalid I/O
+ */
 public static void displayErrorPage(String statusCode, String errorMessage, Request request, Socket clientSocket) throws IOException{
 	String content = new String (Files.readAllBytes( Paths.get(resourceDirectory+File.separator+"errorPage.html")));
 	content = replace(content, request.getReplacesMap());
@@ -146,6 +178,12 @@ public static void displayErrorPage(String statusCode, String errorMessage, Requ
 	write(clientSocket.getOutputStream(),content+"\r\n");
 }
 
+/**
+ * customResponse- overrides default response with custom logic 
+ * @param response- the current response object 
+ * @param clientSocket- current TCP client instance
+ * @throws IOException- if invalid I/O
+ */
 public static void customResponse(Socket clientSocket, String response) throws IOException{
 	write(clientSocket.getOutputStream(),"HTTP/1.0 200 OK\r\n");
 	write(clientSocket.getOutputStream(),"Date: "+new Date()+"\r\n");
@@ -161,6 +199,7 @@ public static void write(OutputStream output, String s) throws IOException {
 	    output.write(s.getBytes());
 }
 
+//Compile page logic at runtime if not present, then run
 public static String compileAndRun(String fileName, Request request, Socket clientSocket)  throws IOException{
 	try {
 		if (!new File(generatedDirectory+File.separator+fileName+".class").exists())
@@ -198,6 +237,7 @@ public static String runClass(String fileName, Request request, Socket clientSoc
 	 }
    }
 
+// Replaces strings with the ${} format with provided replacement 
  public static String replace(String original, HashMap<String, String> replaces){
        Pattern pattern = Pattern.compile("\\$\\{(.*?)}");
        Matcher matcher = pattern.matcher(original);
@@ -216,6 +256,7 @@ public static String runClass(String fileName, Request request, Socket clientSoc
         return builder.toString();
      }
 
+//Will print to console and log to log.txt
 public static void log(String message) throws IOException{
     Writer logger = new BufferedWriter(new FileWriter(
     		new File(System.getProperty("user.dir")).getParent()+File.separator+"log.txt", true));
@@ -225,6 +266,7 @@ public static void log(String message) throws IOException{
 	}
 }
 
+//Helper class to hold/process data about incoming request
 class Request{
 	String method;
 	String resourceRequested;
@@ -234,6 +276,7 @@ class Request{
 	String fileExtension;
 	HashMap<String, String> replacesMap = new HashMap<String, String>();
 	
+        //Setters/getters below
 	public HashMap<String, String> getReplacesMap(){
 		return replacesMap;
 	}
